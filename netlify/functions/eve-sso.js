23bab7a972da8c4ba104f711b492e0b3c56b479a
o7eve <160417158+o7eve@users.noreply.github.com>
2024-02-20 14:03:15 -0600

    Update eve-sso.js

--- netlify/functions/eve-sso.js
+++ netlify/functions/eve-sso.js
@@ -1,1 +1,20 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event) {
+  const { code } = JSON.parse(event.body);
+  const response = await fetch('https://login.eveonline.com/v2/oauth/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `grant_type=authorization_code&code=${code}&client_id=${process.env.EVE_CLIENT_ID}&client_secret=${process.env.EVE_CLIENT_SECRET}`,
+  });
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ accessToken: data.access_token }),
+  };
+};
 
