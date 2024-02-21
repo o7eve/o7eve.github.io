5bdbead9cfa26b6c8ce5fcaf2718e11527372924
o7eve <160417158+o7eve@users.noreply.github.com>
2024-02-20 20:46:51 -0600

    Update get-client-id.js

--- netlify/functions/get-client-id.js
+++ netlify/functions/get-client-id.js
@@ -1,7 +1,11 @@
-// get-client-id.js
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': 'https://o7eve.github.io', // Allow only your GitHub Pages domain
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        },
         body: JSON.stringify({
             clientId: process.env.EVE_CLIENT_ID
         })
