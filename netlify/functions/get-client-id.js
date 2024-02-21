252af377dd00ee3fd5e97d4a62405c67fbef02bd
o7eve <160417158+o7eve@users.noreply.github.com>
2024-02-20 18:04:59 -0600

    Update get-client-id.js

--- netlify/functions/get-client-id.js
+++ netlify/functions/get-client-id.js
@@ -1,1 +1,10 @@
 
+// get-client-id.js
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            clientId: process.env.EVE_CLIENT_ID
+        })
+    };
+};
