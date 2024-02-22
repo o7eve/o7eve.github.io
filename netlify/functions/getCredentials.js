d17b864a96308b3ab1dd490294f1f6a11feb74be
o7eve <160417158+o7eve@users.noreply.github.com>
2024-02-22 11:04:46 -0600

    Update getCredentials.js

--- netlify/functions/getCredentials.js
+++ netlify/functions/getCredentials.js
@@ -1,1 +1,9 @@
-
+exports.handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            clientId: process.env.EVE_CLIENT_ID,
+            clientSecret: process.env.EVE_CLIENT_SECRET
+        })
+    };
+};
