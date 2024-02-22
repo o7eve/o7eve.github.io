7eb41cfa6f74a8d1f4cb60ba990671c27c75be04
o7eve <160417158+o7eve@users.noreply.github.com>
2024-02-22 15:12:52 -0600

    Update getCredentials.js

--- functions/getCredentials.js
+++ functions/getCredentials.js
@@ -1,1 +1,15 @@
+exports.handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': 'https://o7eve.github.io',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        },
+        body: JSON.stringify({
+            clientId: process.env.EVE_CLIENT_ID,
+            clientSecret: process.env.EVE_CLIENT_SECRET
+        })
+    };
+};
 
