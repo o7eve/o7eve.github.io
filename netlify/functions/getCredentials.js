4aa6a6a4789539e28439e936df42ece87cfe46fa
o7eve <160417158+o7eve@users.noreply.github.com>
2024-02-22 11:30:23 -0600

    Update getCredentials.js

--- netlify/functions/getCredentials.js
+++ netlify/functions/getCredentials.js
@@ -1,6 +1,10 @@
 exports.handler = async (event, context) => {
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': 'https://o7eve.github.io',
+            'Access-Control-Allow-Headers': 'Content-Type',
+        },
         body: JSON.stringify({
             clientId: process.env.EVE_CLIENT_ID,
             clientSecret: process.env.EVE_CLIENT_SECRET
